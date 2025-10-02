3c5929424e29bbee5fdfd424bf58b1f333ec3ae1
Tivis7 <106712424+Kevin-REU@users.noreply.github.com>
2025-10-02 12:09:57 -0700

    Hurra!
    This will output my name and some other info!

---
+++ MyName.py
@@ -0,0 +1,20 @@
+#	Kevin Gustavo Roldan
+#	ASTR-19
+#	2025/09/30
+
+
+# This program will write: "Buenos Días Compadre!"
+
+
+# Funtion Tells Comp. to print: "Buenos Días Compadre!"
+def PrintName():
+	print("\n\n Full name: Kevin Gustavo Roldan\n Pronouns: He/Him\n Favorite Movie: Interstellar\n Favorite Food: Mariscos\n\n")
+
+# This defines our main() function for this program
+def main():
+	PrintName()
+
+
+# When we run this program this executes first
+if __name__=="__main__":
+	main()
